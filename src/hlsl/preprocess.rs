968f5c693b2e74bc9203dfd46a4a2caba633a6f6
Trark <trarkium@gmail.com>
2015-12-27 16:43:19 +0000

    preprocess by eating chunks instead of by line
    Now search for the next \n and consume up to there
    and increase line counts instead of using .lines()
    
    This should make it easier to do multi line preprocess
    commands
    
    Writing a string to output now is forced to come with
    a location so they can't get out of sync
    
    We now panic on encountering #if, #else, #endif, #define

--- src/hlsl/preprocess.rs
+++ src/hlsl/preprocess.rs
@@ -37,11 +37,35 @@ pub struct PreprocessedText {
 }
 
 impl PreprocessedText {
+    fn from_intermediate_text(text: IntermediateText) -> PreprocessedText {
+        PreprocessedText {
+            code: text.buffer.into_bytes(),
+            debug_locations: text.debug_locations,
+        }
+    }
     pub fn as_bytes(&self) -> &[u8] {
         &self.code
     }
     pub fn get_file_location(&self, stream_location: &StreamLocation) -> Result<FileLocation, ()> {
         self.debug_locations.get_file_location(stream_location)
+    }
+}
+
+struct IntermediateText {
+    buffer: String,
+    debug_locations: LineMap,
+}
+
+impl IntermediateText {
+    fn new() -> IntermediateText { IntermediateText { buffer: String::new(), debug_locations: LineMap { lines: vec![] } } }
+    fn push_str(&mut self, segment: &str, segment_location: FileLocation) {
+        match segment.find('\n') {
+            Some(sz) => assert_eq!(sz, segment.len() - 1),
+            None => { },
+        }
+        let stream_location_in_buffer = StreamLocation(self.buffer.len() as u64);
+        self.buffer.push_str(segment);
+        self.debug_locations.lines.push((stream_location_in_buffer, segment_location));
     }
 }
 
@@ -69,83 +93,117 @@ impl LineMap {
     }
 }
 
-fn preprocess_file(buffer: String, file_loader: &IncludeHandler, file: &str, debug_locations: &mut Vec<(StreamLocation, FileLocation)>) -> Result<String, PreprocessError> {
-    let lines = file.split('\n').collect::<Vec<_>>();
-    let mut buffer = buffer;
-    for (line_index, line) in lines.iter().enumerate() {
-
-        let trimmed = line.trim_left();
-        if trimmed.starts_with("#") {
-            let trimmed = trimmed[1..].trim_left();
-            if trimmed.starts_with("include") {
-                let next = &trimmed[7..];
-                match next.chars().next() {
-                    Some(' ') | Some('\t') | Some('"') | Some('<') => {
-                        let next = next.trim_left();
-                        let end = match next.chars().next() {
-                            Some('"') => '"',
-                            Some('<') => '>',
-                            _ => return Err(PreprocessError::InvalidInclude),
-                        };
-                        let next = &next[1..];
-                        match next.find(end) {
-                            Some(sz) => {
-                                let file_name = &next[..sz];
-                                // Ignore the rest of the line
-
-                                // Include the file
-                                match file_loader.load(file_name) {
-                                    Ok(file) => {
-                                        buffer = try!(preprocess_file(buffer, file_loader, &file, debug_locations));
-                                        // Push a new line so the last line of the include file is on a
-                                        // separate line to the first line after the #include
-                                        buffer.push('\n');
-                                    },
-                                    Err(()) => return Err(PreprocessError::FailedToFindFile),
-                                }
-                            }
-                            None => return Err(PreprocessError::InvalidInclude),
-                        }
-                    }
-                    _ => return Err(PreprocessError::InvalidInclude),
-                }
-            } else {
-                return Err(PreprocessError::UnknownCommand);
-            }
-
-            // Finish processing the current line
-            continue;
-        }
-
-        // Add the current location to the line map
-        let stream_offset = buffer.as_bytes().len() as u64;
-        let line_number = (line_index + 1) as u64;
-        let column_number = 1; // The first index will display as column 1
-        debug_locations.push((
-            StreamLocation(stream_offset),
-            FileLocation(File::Unknown, Line(line_number), Column(column_number))
-        ));
-
-        // Add line to the preprocessed text buffer
-        buffer.push_str(line);
-        if line_index != lines.len() - 1 {
-            // Add the line end that was removed by the split if we're
-            // not on the last line
-            buffer.push('\n');
+fn build_file_linemap(file_contents: &str, file_name: File) -> LineMap {
+    let mut line_map = LineMap { lines: vec![] };
+    let file_length = file_contents.len() as u64;
+    let mut stream = file_contents;
+    let mut current_line = 1;
+    loop {
+        let (sz, final_segment) = match stream.find("\n") {
+            Some(sz) => (sz + 1, false),
+            None => (stream.len(), true),
+        };
+        let length_left = stream.len() as u64;
+        line_map.lines.push((StreamLocation(file_length - length_left), FileLocation(file_name.clone(), Line(current_line), Column(1))));
+        current_line = current_line + 1;
+        stream = &stream[sz..];
+        if final_segment {
+            break;
         }
     }
-    Ok(buffer)
+    line_map
 }
 
-pub fn preprocess(input: &str, file_loader: &IncludeHandler) -> Result<PreprocessedText, PreprocessError> {
+fn preprocess_command<'a>(buffer: &mut IntermediateText, include_handler: &IncludeHandler, command: &'a str, location: FileLocation) -> Result<&'a str, PreprocessError> {
+    if command.starts_with("include") {
+        let next = &command[7..];
+        match next.chars().next() {
+            Some(' ') | Some('\t') | Some('"') | Some('<') => {
+                let args = next.trim_left();
+                let end = match args.chars().next() {
+                    Some('"') => '"',
+                    Some('<') => '>',
+                    _ => return Err(PreprocessError::InvalidInclude),
+                };
+                let args = &args[1..];
+                match args.find(end) {
+                    Some(sz) => {
+                        let file_name = &args[..sz];
+                        if file_name.contains('\n') {
+                            return Err(PreprocessError::InvalidInclude);
+                        }
 
-    let mut lines = vec![];
-    let code = try!(preprocess_file(String::new(), file_loader, input, &mut lines));
+                        // Include the file
+                        match include_handler.load(file_name) {
+                            Ok(file) => {
+                                try!(preprocess_file(buffer, include_handler, &file));
+                                // Push a new line so the last line of the include file is on a
+                                // separate line to the first line after the #include
+                                buffer.push_str("\n", location);
 
-    Ok(PreprocessedText {
-        code: code.into_bytes(),
-        debug_locations: LineMap { lines: lines },
-    })
+                                Ok(&args[(sz + 1)..])
+                            },
+                            Err(()) => return Err(PreprocessError::FailedToFindFile),
+                        }
+                    }
+                    None => return Err(PreprocessError::InvalidInclude),
+                }
+            }
+            _ => return Err(PreprocessError::InvalidInclude),
+        }
+    } else if command.starts_with("if") {
+        panic!("unimplemented: #if")
+    } else if command.starts_with("else") {
+        panic!("unimplemented: #else")
+    } else if command.starts_with("endif") {
+        panic!("unimplemented: #endif")
+    } else if command.starts_with("define") {
+        panic!("unimplemented: #define")
+    } else {
+        return Err(PreprocessError::UnknownCommand);
+    }
+}
+
+fn preprocess_file(buffer: &mut IntermediateText, include_handler: &IncludeHandler, file: &str) -> Result<(), PreprocessError> {
+
+    let line_map = build_file_linemap(file, File::Unknown);
+    let file_length = file.len() as u64;
+
+    let mut stream = file;
+    loop {
+        let stream_location_in_file = StreamLocation(file_length - stream.len() as u64);
+        let file_location = match line_map.get_file_location(&stream_location_in_file) {
+            Ok(loc) => loc,
+            Err(_) => panic!("could not find line for current position in file"),
+        };
+        let start_trimmed = stream.trim_left();
+        if start_trimmed.starts_with("#") {
+            let command = start_trimmed[1..].trim_left();
+            stream = try!(preprocess_command(buffer, include_handler, command, file_location));
+        } else {
+            // Normal line
+            let (sz, final_segment) = match stream.find('\n') {
+                Some(sz) => (sz + 1, false),
+                None => (stream.len(), true),
+            };
+            let line = &stream[..sz];
+            buffer.push_str(line, file_location);
+            stream = &stream[sz..];
+            if final_segment {
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+pub fn preprocess(input: &str, include_handler: &IncludeHandler) -> Result<PreprocessedText, PreprocessError> {
+
+    let mut intermediate_text = IntermediateText::new();
+    try!(preprocess_file(&mut intermediate_text, include_handler, input));
+
+    Ok(PreprocessedText::from_intermediate_text(intermediate_text))
 }
 
 pub fn preprocess_single(input: &str) -> Result<PreprocessedText, PreprocessError> {
